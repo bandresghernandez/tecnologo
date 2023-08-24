51ccc7f9cd45c751c4311d2d7b072b95f898aaa6
bandresghernandez <bandresghernandez@gmail.com>
2023-08-24 20:36:05 -0300

    updates

--- src/main/java/logica/Socio.java
+++ src/main/java/logica/Socio.java
@@ -1,9 +1,28 @@
 package logica;
 
+import java.util.ArrayList;
 import datatypes.DtFecha;
 
+
 class Socio extends Usuario {
+	
+	 private ArrayList<Registro> registros;
+
+	
     public Socio(String nickname, String nombre, String apellido, String email, DtFecha fechaNac) {
         super(nickname, nombre, apellido, email, fechaNac);
     }
+    
+    
+    // Getter and setter for the 'registros' attribute
+    public ArrayList<Registro> getRegistros() {
+        return registros;
+    }
+
+    public void setRegistros(ArrayList<Registro> registros) {
+        this.registros = registros;
+    }
 }
+
+
+
