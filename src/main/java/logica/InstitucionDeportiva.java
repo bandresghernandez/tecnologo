2ee86d1c5535471c6314bb36dd6b8929ba6f2260
bandresghernandez <bandresghernandez@gmail.com>
2023-08-24 14:38:50 -0300

    update instituciondeportiva

--- src/main/java/logica/InstitucionDeportiva.java
+++ src/main/java/logica/InstitucionDeportiva.java
@@ -1,18 +1,26 @@
 package logica;
+import java.util.ArrayList;
+import java.util.List;
+
+import logica.ActividadDeportiva;
 
 public class InstitucionDeportiva {
     private String nombre;
     private String descripcion;
     private String url;
+    private List<ActividadDeportiva> actividades;
+
 
     // Constructor
-    public InstitucionDeportiva(String nombre, String descripcion, String url) {
+    public InstitucionDeportiva(String nombre, String descripcion, String url, List<ActividadDeportiva> actividades) {
         this.nombre = nombre;
         this.descripcion = descripcion;
         this.url = url;
+        this.actividades = actividades;
     }
+    
+ // Getters y setters para los atributos
 
-    // Métodos getter y setter para nombre
     public String getNombre() {
         return nombre;
     }
@@ -21,7 +29,6 @@ public class InstitucionDeportiva {
         this.nombre = nombre;
     }
 
-    // Métodos getter y setter para descripcion
     public String getDescripcion() {
         return descripcion;
     }
@@ -30,7 +37,6 @@ public class InstitucionDeportiva {
         this.descripcion = descripcion;
     }
 
-    // Métodos getter y setter para url
     public String getUrl() {
         return url;
     }
@@ -39,18 +45,55 @@ public class InstitucionDeportiva {
         this.url = url;
     }
 
-    // Método para mostrar información de la institución
-    public void mostrarInformacion() {
-        System.out.println("Nombre: " + nombre);
-        System.out.println("Descripción: " + descripcion);
-        System.out.println("URL: " + url);
+    public List<ActividadDeportiva> getActividades() {
+        return actividades;
+    }
+
+    public void setActividades(List<ActividadDeportiva> actividades) {
+        this.actividades = actividades;
     }
 
     public static void main(String[] args) {
+
+        // Crear una lista de actividades deportivas
+       List<ActividadDeportiva> actividades = new ArrayList<>();
+        actividades.add(new ActividadDeportiva("Fútbol", "de futbol",0, 0, null));
+        actividades.add(new ActividadDeportiva("Yoga", "de YOGA",0, 0, null));
+       
+
+        // Crear una instancia de InstitucionDeportiva utilizando el constructor
+        InstitucionDeportiva institucion = new InstitucionDeportiva("Deportes Total",
+                "Centro deportivo con varias instalaciones",
+                "http://www.deportestotal.com",
+                actividades);
+
+        // Imprimir información de la institución
+        System.out.println("Nombre: " + institucion.getNombre());
+        System.out.println("Descripción: " + institucion.getDescripcion());
+        System.out.println("URL: " + institucion.getUrl());
+        System.out.println("Actividades:");
+
+        for (ActividadDeportiva actividad : institucion.getActividades()) {
+            System.out.println("- " + actividad.getNombre());
+        }
+    }
+    
+    
+    
+    
+
+    // Método para mostrar información de la institución
+//    public void mostrarInformacion() {
+//        System.out.println("Nombre: " + nombre);
+//        System.out.println("Descripción: " + descripcion);
+ //       System.out.println("URL: " + url);
+  //  }
+
+  //  public static void main(String[] args) {
         // Crear una instancia de InstitucionDeportiva
-        InstitucionDeportiva institucion = new InstitucionDeportiva("Club Deportivo XYZ", "Club deportivo para practicar varios deportes", "http://www.clubdeportivoxyz.com");
+    //    InstitucionDeportiva institucion = new InstitucionDeportiva("Club Deportivo XYZ", "Club deportivo para practicar varios deportes", "http://www.clubdeportivoxyz.com");
 
         // Mostrar información de la institución
-        institucion.mostrarInformacion();
-    }
+      //  institucion.mostrarInformacion();
+    //}
 }
