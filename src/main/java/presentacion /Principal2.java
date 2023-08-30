525224b70fc4f277303780ec94db0220fe9b57ea
bandresghernandez <48714822+bandresghernandez@users.noreply.github.com>
2023-08-30 16:09:51 -0300

    Update Principal2.java

--- src/main/java/presentacion /Principal2.java
+++ src/main/java/presentacion /Principal2.java
@@ -1,1 +1,115 @@
+package presentacion;
+
+import java.awt.EventQueue;
+
+public class Principal2 {
+
+    private JFrame frame;
+    private JDesktopPane desktopPane;
+
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    Principal2 window = new Principal2();
+                    window.frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    public Principal2() {
+        initialize();
+    }
+
+    private void initialize() {
+        frame = new JFrame();
+        frame.setBounds(100, 100, 800, 600);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        desktopPane = new JDesktopPane();
+        frame.getContentPane().add(desktopPane);
+
+        JMenuBar menuBar = new JMenuBar();
+        frame.setJMenuBar(menuBar);
+
+        JMenu mnAltas = new JMenu("Altas");
+        menuBar.add(mnAltas);
+
+        JMenuItem mntmUsuario = new JMenuItem("Usuario");
+        mntmUsuario.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                IngresarUsuario2 ingresoUsuario = new IngresarUsuario2();
+                desktopPane.add(ingresoUsuario);
+                ingresoUsuario.setVisible(true);
+            }
+        });
+        mnAltas.add(mntmUsuario);
+        
+        JMenuItem mntmInstDeportiva = new JMenuItem("Inst. Deportiva");
+        mntmInstDeportiva.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+            	IngresarInstitucionDeportiva2 ingresoInst = new IngresarInstitucionDeportiva2();
+                desktopPane.add(ingresoInst);
+                ingresoInst.setVisible(true);
+            }
+        });
+        
+        //IngresarActividadDeportiva2
+        
+        mnAltas.add(mntmInstDeportiva);
+        
+        
+        
+        
+        
+        
+        JMenuItem mntmActDeportivaa = new JMenuItem("Act. Deportiva");
+        mntmActDeportivaa.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+            	IngresarActividadDeportiva2 a = new IngresarActividadDeportiva2();
+                desktopPane.add(a);
+                a.setVisible(true);
+            }
+        });
+        
+        
+        
+        mnAltas.add(mntmActDeportivaa);
+        
+        
+        
+        JMenuItem mntmClase = new JMenuItem("Clase");
+        mntmClase.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+            	IngresarClase2 B = new IngresarClase2();
+                desktopPane.add(B);
+                B.setVisible(true);
+            }
+        });
+        
+        
+        
+        mnAltas.add(mntmClase);
+        
+        JMenu mnInformacion = new JMenu("Información");
+        menuBar.add(mnInformacion);
+        
+        
+        
+        JMenuItem mntmSocio = new JMenuItem("Socios");
+        
+        mntmSocio.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+            	InfoSocios a = new InfoSocios();
+                desktopPane.add(a);
+                a.setVisible(true);
+            }
+        });
+        
+        mnInformacion.add(mntmSocio);
+    }
+}
 
