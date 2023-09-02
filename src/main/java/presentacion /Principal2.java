d6c128f0874e5ecc354671b70c7f5501c72e3ddd
bandresghernandez <48714822+bandresghernandez@users.noreply.github.com>
2023-09-02 15:40:31 -0300

    Update Principal2.java

--- src/main/java/presentacion /Principal2.java
+++ src/main/java/presentacion /Principal2.java
@@ -1,5 +1,13 @@
 package presentacion;
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JFrame;
+import javax.swing.JDesktopPane;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import java.awt.EventQueue;
 
 public class Principal2 {
