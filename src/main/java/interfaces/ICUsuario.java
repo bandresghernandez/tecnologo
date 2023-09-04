70c539e85f13e2910e713374455fb5600d6a5627
Agus0298 <141374099+Agus0298@users.noreply.github.com>
2023-09-04 15:31:03 -0300

    Update ICUsuario.java
    descomento las funciones usadas

--- src/main/java/interfaces/ICUsuario.java
+++ src/main/java/interfaces/ICUsuario.java
@@ -1,6 +1,7 @@
 package interfaces;
 
 import java.util.ArrayList;
+
 import java.util.Set;
 
 import datatypes.DtUsuario;
@@ -8,33 +9,8 @@ import excepciones.UsuarioEnUsoExcepcion;
 
 public interface ICUsuario {
 	public void agregarUsuario(DtUsuario usuario) throws UsuarioEnUsoExcepcion;
-	
 	public DtUsuario obtenerUsuario(String nickname);// en pav usabamos el main para iterar y mostrar toda la info de usuario
-	
-	
-	
-	/*
-	   // public ArrayList<DtSocio> listarSocios ();
-
-			
-		//public void selectDatos (String socio, String clase, DtFecha FechaReg, float costo);
-
-	//public void listarSocio(); 
+	public Set<String> listarUsuarios();
 	public Set<String> listarSocios();
-
-
-	
- //  public Set<DtSocio> listarSocio ();
-		
-	public void ingresarSocio (String nickname, String nombre, String apellido, String email, DtFecha fechaNac) ;
-
-			
-	//	public void selectUsuario (String usuario);
-					
-
-		//public void ingresarProfesor (String nickname, String nombre, String apellido, String email,
-          //      DtFecha fechaNac, String descripcion, String biografia, String sitioweb);
-
-
-	*/	
+	//public boolean selecDatos(String socio, String clase, DtFecha fechaReg, Float costo);
 	}
