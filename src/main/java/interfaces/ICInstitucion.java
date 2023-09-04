82e97c36bf27d5e5df1f97663467b3d9b47edb2a
Agus0298 <141374099+Agus0298@users.noreply.github.com>
2023-09-04 15:17:07 -0300

    Update ICInstitucion.java
    agrego throws InstitucionRepetidaExcepcion

--- src/main/java/interfaces/ICInstitucion.java
+++ src/main/java/interfaces/ICInstitucion.java
@@ -7,13 +7,14 @@ import datatypes.DtActividadDeportiva;
 import datatypes.DtClase;
 import datatypes.DtFecha;
 import datatypes.DtHora;
+import excepciones.InstitucionRepetidaExcepcion;
 
 public interface ICInstitucion {
 
     public ArrayList<String> listarInstitucionDeportiva();
     public Set<String> selectInstitucionDeportiva(String institucion);
     public DtActividadDeportiva selectActividadDeportiva(String actividad);
-    public boolean altaInstitucionDeportiva(String nombre_institucion, String descripcion, String url);
+    public boolean altaInstitucionDeportiva(String nombre_institucion, String descripcion, String url)throws InstitucionRepetidaExcepcion;
     public boolean altaClase(String actividad, String nombre, DtFecha fechaini, DtHora horaini, String profesor, String url, DtFecha fechaAlta);
     public boolean altaActividadDeportiva(String nombre_institucion, String nombre, String descripcion, int duracion, Float costo, DtFecha fechaAlta);
     public DtClase selectClase(String nombre);
