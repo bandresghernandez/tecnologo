45e12cd3be0250b70187581873651a09dfa96712
Ximena205 <ximee.gonzalez91@gmail.com>
2023-09-03 21:09:48 -0300

    subo paquete de excepciones con la excepcion de usuarioen uso

---
+++ src/main/java/excepciones/UsuarioEnUsoExcepcion.java
@@ -0,0 +1,13 @@
+package excepciones;
+
+public class UsuarioEnUsoExcepcion extends Exception{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	public UsuarioEnUsoExcepcion(String string) {
+		super(string);
+	}
+
+}
