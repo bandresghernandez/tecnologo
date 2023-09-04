e47daf9e3f86f47f4e6268b879d0848283c23a36
Agus0298 <141374099+Agus0298@users.noreply.github.com>
2023-09-04 15:15:24 -0300

    Create InstitucionRepetidaExcepcion.java
    Agrego InstitucionRepetidaExcepcion

---
+++ src/main/java/excepciones/InstitucionRepetidaExcepcion.java
@@ -0,0 +1,9 @@
+package Excepciones;
+
+public class InstitucionRepetidaExcepcion extends Exception{
+	private static final long serialVersionUID = 1L;
+
+	public InstitucionRepetidaExcepcion(String string) {
+        super(string);
+    }
+}
