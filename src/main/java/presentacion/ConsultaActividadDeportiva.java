2170272b74f38bbb87865cbf50f45af484190e1c
Luciano <e.gonzalez0024@gmail.com>
2023-09-05 00:17:39 -0300

    agrego listar institutos ConsultaActividadDepoFrame

--- src/main/java/presentacion/ConsultaActividadDeportiva.java
+++ src/main/java/presentacion/ConsultaActividadDeportiva.java
@@ -1,6 +1,7 @@
 package presentacion;
 
 import java.awt.EventQueue;
+import java.util.ArrayList;
 
 import javax.swing.JFrame;
 import javax.swing.JInternalFrame;
@@ -12,6 +13,9 @@ import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.JTextPane;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class ConsultaActividadDeportiva extends JInternalFrame {
 
@@ -32,49 +36,72 @@ public class ConsultaActividadDeportiva extends JInternalFrame {
 		setResizable(true);//permite cambiar el tamaño del jframe
         setIconifiable(true);//permite poner iconos
         setMaximizable(true);//permite maximizar y minimizar el jframe
-        setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);// se oculta jframe en lugar de cerrarse
+        //setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);// se oculta jframe en lugar de cerrarse
         setClosable(true);// permite que el usuario cierre la ventana
-        setTitle("Alta de una Institucion");//setteo el titulo
+        setTitle("Constula Activdad Deportiva");//setteo el titulo
 		
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 800, 600);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
 		JComboBox comboBoxInstituto = new JComboBox();
-		comboBoxInstituto.setBounds(10, 34, 194, 22);
+		comboBoxInstituto.setBounds(10, 57, 328, 22);	
 		contentPane.add(comboBoxInstituto);
 		
+		
 		JComboBox comboBoxActividad = new JComboBox();
-		comboBoxActividad.setBounds(10, 92, 194, 22);
+		comboBoxActividad.setBounds(10, 152, 328, 22);
 		contentPane.add(comboBoxActividad);
 		
 		JComboBox comboBoxClase = new JComboBox();
-		comboBoxClase.setBounds(10, 150, 194, 22);
+		comboBoxClase.setBounds(10, 254, 328, 22);
 		contentPane.add(comboBoxClase);
 		
 		JLabel lblComboInstituto = new JLabel("Institutos");
-		lblComboInstituto.setBounds(10, 11, 46, 14);
+		lblComboInstituto.setBounds(10, 32, 194, 14);
 		contentPane.add(lblComboInstituto);
 		
 		JLabel lblComboActividad = new JLabel("Actividades");
-		lblComboActividad.setBounds(10, 67, 64, 14);
+		lblComboActividad.setBounds(10, 127, 64, 14);
 		contentPane.add(lblComboActividad);
 		
 		JLabel lblComboClase = new JLabel("Clases");
-		lblComboClase.setBounds(10, 125, 46, 14);
+		lblComboClase.setBounds(10, 216, 46, 14);
 		contentPane.add(lblComboClase);
 		
 		JTextPane textInfoClase = new JTextPane();
+		textInfoClase.setEnabled(false);
 		textInfoClase.setText("Detalles de las clase . . .");
-		textInfoClase.setBounds(10, 196, 414, 63);
+		textInfoClase.setBounds(10, 357, 747, 141);
 		contentPane.add(textInfoClase);
 		
 		JTextPane textInfoActividad = new JTextPane();
+		textInfoActividad.setEnabled(false);
 		textInfoActividad.setText("Informacion de Actividad . . .");
-		textInfoActividad.setBounds(214, 34, 210, 140);
+		textInfoActividad.setBounds(385, 32, 372, 314);
 		contentPane.add(textInfoActividad);
+		
+		JButton btnRecargar = new JButton("Recargar");
+		btnRecargar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				comboBoxInstituto.removeAllItems();
+				ArrayList<String> instituciones = iinst.listarInstitucionDeportiva();
+				// miro si me trae algo
+				for (String s : instituciones) {
+					System.out.print(s);
+				}
+				
+				for (String s : instituciones) {
+					comboBoxInstituto.addItem(s);
+				}
+			comboBoxInstituto.addItem("cosa 1");
+			}
+		});
+		btnRecargar.setBounds(10, 303, 89, 23);
+		contentPane.add(btnRecargar);
 		
 	}
 }
