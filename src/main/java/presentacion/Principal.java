4480848544c7b2019844747c7c3d8c24595e4e66
bandresghernandez <48714822+bandresghernandez@users.noreply.github.com>
2023-09-06 10:55:36 -0300

    Update Principal.java

--- src/main/java/presentacion/Principal.java
+++ src/main/java/presentacion/Principal.java
@@ -14,18 +14,37 @@ import interfaces.Fabrica;
 import interfaces.ICInstitucion;
 import interfaces.ICUsuario;
 
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+//import javax.swing.JDesktopPane;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import java.awt.Dimension;
+
+import interfaces.Fabrica;
+import interfaces.ICInstitucion;
+import interfaces.ICUsuario;
+import java.awt.BorderLayout;
+
 public class Principal {
 
     private JFrame frame;//frame principal
     
-    private JDesktopPane desktopPane;
-    private IngresarInstitucionDeportiva ingresarInstDepInternalFrame;
-    private ConsultaActividadDeportiva consultaActividadDeportivaFrame;
+    //private JDesktopPane desktopPane;
+    //private IngresarInstitucionDeportiva ingresarInstDepInternalFrame;
+    
+    private IngresarUsuario ingresarUsuarioInternalFrame;
+	private IngresarInstitucionDeportiva ingresarInstDepInternalFrame;
+	private IngresarActividadDeportiva ingresarActividadDeportivaInternalFrame;
+	private IngresarClase ingresarClaseInternalFrame;
+	private ConsultaUsuario consultaUsuarioInternalFrame;
+	private ConsultaActividadDeportiva consultaActividadDepInternalFrame;
     private IngresarRegistro IngresarRegistroFrame;
-	private IngresarUsuario ingresarUsuarioInternalFrame;
-   private ConsultaUsuario consultaUsuarioInternalFrame;
-	private IngresarActividadDeportiva ingresarActividadDepInternalFrame;
-
+	
     public static void main(String[] args) {
         EventQueue.invokeLater(new Runnable() {
             public void run() {
@@ -48,65 +67,71 @@ public class Principal {
         
         Dimension desktopSize = frame.getSize();
 		Dimension jInternalFrameSize;
-
-	    //Ingresar Usuario
-        ingresarUsuarioInternalFrame = new IngresarUsuario(iusu);
+		
+		ingresarUsuarioInternalFrame = new IngresarUsuario(iusu);
 		jInternalFrameSize = ingresarUsuarioInternalFrame.getSize();
 		ingresarUsuarioInternalFrame.setLocation((desktopSize.width - jInternalFrameSize.width)/2, (desktopSize.height- jInternalFrameSize.height)/2);
 		//ingresarUsuarioInternalFrame.setSize(450, 300);
 		ingresarUsuarioInternalFrame.setVisible(false);
 		frame.getContentPane().add(ingresarUsuarioInternalFrame);
 		//ingresarInstDepInternalFrame.getContentPane().add(ingresarUsuarioInternalFrame);
-	    
-		//Ingresar Institucion Deportiva		
+		
+		
         ingresarInstDepInternalFrame = new IngresarInstitucionDeportiva(iinst);//lo instancio
         jInternalFrameSize = ingresarInstDepInternalFrame.getSize();
         ingresarInstDepInternalFrame.setLocation((desktopSize.width - jInternalFrameSize.width)/2,//lo pongo en el medio de la pantalla
 		    (desktopSize.height- jInternalFrameSize.height)/2);
         ingresarInstDepInternalFrame.setVisible(false);//lo hago invisible
 		frame.getContentPane().add(ingresarInstDepInternalFrame);//lo agrego
-//Consulta Usuario
-consultaUsuarioInternalFrame = new ConsultaUsuario(iusu);//lo instancio
+		
+		
+		
+		
+		ingresarActividadDeportivaInternalFrame = new IngresarActividadDeportiva(iinst);//lo instancio
+        jInternalFrameSize = ingresarActividadDeportivaInternalFrame.getSize();
+        ingresarActividadDeportivaInternalFrame.setLocation((desktopSize.width - jInternalFrameSize.width)/2,//lo pongo en el medio de la pantalla
+		    (desktopSize.height- jInternalFrameSize.height)/2);
+        ingresarActividadDeportivaInternalFrame.setVisible(false);//lo hago invisible
+		frame.getContentPane().add(ingresarActividadDeportivaInternalFrame);//lo agrego
+		
+		ingresarClaseInternalFrame = new IngresarClase();//lo instancio
+        jInternalFrameSize = ingresarClaseInternalFrame.getSize();
+        ingresarClaseInternalFrame.setLocation((desktopSize.width - jInternalFrameSize.width)/2,//lo pongo en el medio de la pantalla
+		    (desktopSize.height- jInternalFrameSize.height)/2);
+        ingresarClaseInternalFrame.setVisible(false);//lo hago invisible
+		frame.getContentPane().add(ingresarClaseInternalFrame, BorderLayout.NORTH);
+		
+		consultaUsuarioInternalFrame = new ConsultaUsuario(iusu);//lo instancio
         jInternalFrameSize = consultaUsuarioInternalFrame.getSize();
         consultaUsuarioInternalFrame.setLocation((desktopSize.width - jInternalFrameSize.width)/2,//lo pongo en el medio de la pantalla
 		    (desktopSize.height- jInternalFrameSize.height)/2);
         consultaUsuarioInternalFrame.setVisible(false);//lo hago invisible
 		frame.getContentPane().add(consultaUsuarioInternalFrame, BorderLayout.NORTH);
-
-	    //Alta Actividad Deportiva
-	    ingresarActividadDepInternalFrame = new IngresarActividadDeportiva(iinst);
-		jInternalFrameSize = ingresarActividadDepInternalFrame.getSize();
-		ingresarActividadDepInternalFrame.setLocation((desktopSize.width - jInternalFrameSize.width)/2, (desktopSize.height- jInternalFrameSize.height)/2);
-		ingresarActividadDepInternalFrame.setVisible(false);
-		frame.getContentPane().add(ingresarActividadDepInternalFrame);
-	    
-		//Consulta Actividad Deportiva
-		consultaActividadDeportivaFrame = new ConsultaActividadDeportiva(iinst);//lo instancio
-        jInternalFrameSize = consultaActividadDeportivaFrame.getSize();
-        consultaActividadDeportivaFrame.setLocation((desktopSize.width - jInternalFrameSize.width)/2,//lo pongo en el medio de la pantalla
+		
+		consultaActividadDepInternalFrame = new ConsultaActividadDeportiva(iinst);//lo instancio
+        jInternalFrameSize = consultaActividadDepInternalFrame.getSize();
+        consultaActividadDepInternalFrame.setLocation((desktopSize.width - jInternalFrameSize.width)/2,//lo pongo en el medio de la pantalla
 		    (desktopSize.height- jInternalFrameSize.height)/2);
-        consultaActividadDeportivaFrame.setVisible(false);//lo hago invisible
-		frame.getContentPane().add(consultaActividadDeportivaFrame);//lo agrego
-	
-	    //Ingresar registro
-
-IngresarRegistroFrame = new IngresarRegistro(iusu);//lo instancio
+        consultaActividadDepInternalFrame.setVisible(false);//lo hago invisible
+		frame.getContentPane().add(consultaActividadDepInternalFrame, BorderLayout.NORTH);
+    
+		//registro
+		IngresarRegistroFrame = new IngresarRegistro(iusu);//lo instancio
         jInternalFrameSize = IngresarRegistroFrame.getSize();
         IngresarRegistroFrame.setLocation((desktopSize.width - jInternalFrameSize.width)/2,//lo pongo en el medio de la pantalla
 		    (desktopSize.height- jInternalFrameSize.height)/2);
         IngresarRegistroFrame.setVisible(false);//lo hago invisible
 		frame.getContentPane().add(IngresarRegistroFrame);//lo agrego
-
-	    
+    
     }
 
     private void initialize() {
         frame = new JFrame();
-        frame.setBounds(100, 100, 800, 600);
+        frame.setBounds(200, 200, 900, 600);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        desktopPane = new JDesktopPane();
-        frame.getContentPane().add(desktopPane);
+        frame.getContentPane().setLayout(null);
+        //desktopPane = new JDesktopPane();
+        //frame.getContentPane().add(desktopPane);
 
         JMenuBar menuBar = new JMenuBar();
         frame.setJMenuBar(menuBar);
@@ -114,7 +139,7 @@ IngresarRegistroFrame = new IngresarRegistro(iusu);//lo instancio
         JMenu mnAltas = new JMenu("Altas");
         menuBar.add(mnAltas);
 
-         JMenuItem mntmUsuario = new JMenuItem("Usuario");
+        JMenuItem mntmUsuario = new JMenuItem("Usuario");
         mntmUsuario.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 //IngresarUsuario ingresoUsuario = new IngresarUsuario();
@@ -141,9 +166,7 @@ IngresarRegistroFrame = new IngresarRegistro(iusu);//lo instancio
         JMenuItem mntmActDeportivaa = new JMenuItem("Act. Deportiva");
         mntmActDeportivaa.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-            	IngresarActividadDeportiva a = new IngresarActividadDeportiva();
-                desktopPane.add(a);
-                a.setVisible(true);
+            	ingresarActividadDeportivaInternalFrame.setVisible(true);
             }
         });
         
@@ -156,9 +179,7 @@ IngresarRegistroFrame = new IngresarRegistro(iusu);//lo instancio
         JMenuItem mntmClase = new JMenuItem("Clase");
         mntmClase.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-            	IngresarClase B = new IngresarClase();
-                desktopPane.add(B);
-                B.setVisible(true);
+            	ingresarClaseInternalFrame.setVisible(true);
             }
         });
         
@@ -168,45 +189,80 @@ IngresarRegistroFrame = new IngresarRegistro(iusu);//lo instancio
         
         JMenu mnInformacion = new JMenu("Información");
         menuBar.add(mnInformacion);
-
-	   JMenuItem mntmSocioInfo = new JMenuItem("Socio");
+        
+        JMenuItem mntmSocioInfo = new JMenuItem("Socio");
         mntmSocioInfo.addActionListener(new ActionListener() {
         	public void actionPerformed(ActionEvent e) {
         		consultaUsuarioInternalFrame.setVisible(true);
         		consultaUsuarioInternalFrame.iniciarlizarComboBoxes();
         	}
         });
+        
+        
         mnInformacion.add(mntmSocioInfo);
-	    
-        JMenuItem mntmConsutlaActividad = new JMenuItem("Consulta Actividad Deportiva");
-        mntmConsutlaActividad.addActionListener(new ActionListener() {
+        
+        JMenuItem mntmActividadDepInfo = new JMenuItem("ActividadDeportiva");
+        mntmActividadDepInfo.addActionListener(new ActionListener() {
         	public void actionPerformed(ActionEvent e) {
-        		consultaActividadDeportivaFrame.setVisible(true);
+        		consultaActividadDepInternalFrame.setVisible(true);
+        		//iniciarlizarComboBoxes
         	}
         });
-        mnInformacion.add(mntmConsutlaActividad);
+        mnInformacion.add(mntmActividadDepInfo);
         
-  
-        JMenu mnRegistro = new JMenu("Registro");
         
-   
+        
+        
+      JMenu mnRegistro = new JMenu("Registro");
+        
+        
         menuBar.add(mnRegistro);
         
         JMenuItem mntmAgregar = new JMenuItem("Agregar");
         mntmAgregar.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
             	IngresarRegistro l = new IngresarRegistro(null);
-                desktopPane.add(l);
-                IngresarRegistroFrame.setVisible(true);
+                 //desktopPane.add(l);
+                //IngresarRegistroFrame.setVisible(true);
+            	IngresarRegistroFrame.setVisible(true);
+
             }
         });
         mnRegistro.add(mntmAgregar);
         
-
-	    
         
-        JMenuItem mntmSocio = new JMenuItem("Socios");
+        
+        
+        
+        JMenu mnSalir = new JMenu("Salir");
+        mnSalir.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        
+        	//falta implementar
+        		
+        	}
+        });
+        menuBar.add(mnSalir);
+        
+        JMenuItem mntmSalirSistema = new JMenuItem("Salir sistema");
+        mnSalir.add(mntmSalirSistema);
+
+        
+        frame.setLocationRelativeTo(null);
+        
+        
+        mntmSalirSistema.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                // Agrega aquí cualquier código adicional que desees ejecutar antes de salir.
+                // Por ejemplo, guardar datos, cerrar conexiones, etc.
+                System.exit(0); // Cierra la aplicación
+            }
+        });
+
+  
         /*
+        JMenuItem mntmSocio = new JMenuItem("Socios");
+    
         mntmSocio.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
             	InfoSocios a = new InfoSocios();
@@ -219,4 +275,6 @@ IngresarRegistroFrame = new IngresarRegistro(iusu);//lo instancio
     }*/
 }
 }
+
+
 
